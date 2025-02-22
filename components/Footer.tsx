55f23c00de82996068c0044b865b5b4b45eeadbb
nik123-py <nik488151@gmail.com>
2025-02-22 21:50:36 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-blue-900 py-8 px-4">
       <div className="container mx-auto text-center">
-        <p>&copy; 2023 MedCaché. All rights reserved.</p>
+        <p>&copy; 2025 MedCaché. All rights reserved.</p>
         <div className="mt-4">
           <a href="#" className="text-blue-300 hover:text-blue-100 mx-2">
             Privacy Policy
